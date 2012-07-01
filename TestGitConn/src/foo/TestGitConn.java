a94d3e89f17f03d45d467163bc2f2bba7d4255f8
kevin wright <wrightkevin2000@gmail.com>
2012-07-01 17:28:09 +0100

    first cut

---
+++ TestGitConn/src/foo/TestGitConn.java
@@ -0,0 +1,14 @@
+package foo;
+
+public class TestGitConn {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("startx");
+
+	}
+
+}
